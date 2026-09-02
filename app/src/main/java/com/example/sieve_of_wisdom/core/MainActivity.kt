97eb8ce60b8b595f7b43997f3e8561ff67c029a3
bao2434 <ncbao2434@apcs.fitus.edu.vn>
2026-09-02 22:16:38 +0700

    meow

--- app/src/main/java/com/example/sieve_of_wisdom/core/MainActivity.kt
+++ app/src/main/java/com/example/sieve_of_wisdom/core/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.fragment_store)
+        setContentView(R.layout.activity_detail)
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
